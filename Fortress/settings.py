e3df5c73a58fbed16185b5d673eb881ef786034e
Samuel Mbano <121202097+samzypaul@users.noreply.github.com>
2025-12-22 18:48:59 +0000

    collecting stating files

--- Fortress/settings.py
+++ Fortress/settings.py
@@ -144,6 +144,7 @@ STATIC_URL = 'static/'
 #STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # STATIC_ROOT = BASE_DIR / 'static'
 # STATICFILES_DIRS = [
 #     BASE_DIR / "static",
